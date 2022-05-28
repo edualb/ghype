44a856594adbeb9c67d547b28ce4f1c3c5acf976
edualb <eduardoslafiq@gmail.com>
2022-05-28 13:45:58 -0300

    (messageformat): create structures
    - startLine structure
    - requestLine structure with implementation of line interface
    - statusLine structure with implementation of line interface
    - line interface with Get() string function

--- internal/messageformat/messageformat.go
+++ internal/messageformat/messageformat.go
@@ -1,5 +1,47 @@
 package messageformat
 
+import "github.com/edualb/ghype/internal/version"
+
 func GetHTTPMessage() string {
+	return ""
+}
+
+type HTTPMessage struct {
+	startline startLine
+}
+
+// requestLine will get requestLine or statusLine
+type startLine struct {
+	requestLine requestLine
+	statusLine  statusLine
+}
+
+func (sl startLine) Get() line {
+	if sl.requestLine.status {
+		return sl.requestLine
+	}
+	return sl.statusLine
+}
+
+type line interface {
+	Get() string
+}
+
+type requestLine struct {
+	method        string // token https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.6
+	requestTarget string //https://datatracker.ietf.org/doc/html/rfc7230#section-5.3
+	httpVersion   string
+	status        bool
+}
+
+func (l requestLine) Get() string {
+	return " " + " " + version.HTTPVersion() + ""
+}
+
+type statusLine struct {
+	status bool
+}
+
+func (l statusLine) Get() string {
 	return ""
 }
